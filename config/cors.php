fa95d2e69dfb8666a5d897842c6a4fbca9d037c2
saithduran <saithelmejor@gmail.com>
2025-03-13 00:21:56 -0500

    Cambio en cors

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173'], // O el dominio de tu frontend
+    'allowed_origins' => ['*'], // O el dominio de tu frontend
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
