f8eaf9a07285cdf1da1812abae5e155b0c59eb8b
saithduran <52015687+saithduran@users.noreply.github.com>
2025-03-15 16:55:33 -0500

    Update Dashboard.jsx
    Cambio al importar navbar

--- resources/js/pages/Dashboard.jsx
+++ resources/js/pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Navbar from '../components/navbar';
+import Navbar from '../components/Navbar';
 import Calendario from '../components/calendario'
 import axios from 'axios';
 
